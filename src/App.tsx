7726623353d88ec42ecc8745b4fda880e7e2138c
Samuel Silva <samuelsilvawb@gmail.com>
2022-07-19 20:41:29 +0100

    refactor: move people component to pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import People from "components/people";
+import People from "pages/people";
+import RegisterPerson from "pages/register-person";
 import Layout from "components/layout";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<People />} />
+        <Route path="/create" element={<RegisterPerson />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/register-person/RegisterPerson.tsx
@@ -0,0 +1,38 @@
+import Container, {
+  CONTAINER_PADDING,
+  CONTAINER_DIRECTION,
+} from "components/container";
+import Spacing, { SPACING_MARGINS } from "components/spacing";
+import { Link } from "react-router-dom";
+
+const RegisterPerson = () => {
+  return (
+    <Container fullHeight fullWidth direction={CONTAINER_DIRECTION.VERTICAL}>
+      <Container
+        fullWidth
+        padding={CONTAINER_PADDING.NONE}
+        spreadContent
+        alignCenter
+      >
+        <Container
+          direction={CONTAINER_DIRECTION.VERTICAL}
+          padding={CONTAINER_PADDING.NONE}
+        >
+          <h1>Register a new member</h1>
+        </Container>
+        <Link to="/">All members</Link>
+      </Container>
+      <Container
+        fullWidth
+        fullHeight
+        alignCenter
+        direction={CONTAINER_DIRECTION.VERTICAL}
+        padding={CONTAINER_PADDING.NONE}
+      >
+        <Spacing size={SPACING_MARGINS.BIG} />
+      </Container>
+    </Container>
+  );
+};
+
+export default RegisterPerson;
